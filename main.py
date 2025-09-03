aca4416bdd7551ba2a0ca1f3b414b26f196c0204
Asad Ali <141873003+Asad-In-Data@users.noreply.github.com>
2025-09-04 01:58:02 +0500

    Refactor to analyze sentiment from local text file
    Commented out the code for summarizing online articles and replaced it with logic to read text from 'my_text.txt' and analyze its sentiment using TextBlob. Added 'my_text.txt' containing sample positive and negative messages for sentiment analysis.

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 from newspaper import Article
 
-import nltk
-nltk.download('punkt_tab')
+# this is for article summarization
+# import nltk
+# nltk.download('punkt')
 
-url = 'https://en.wikipedia.org/wiki/English_language'
-article = Article(url)
-article.download()
-article.parse()
-article.nlp()
+# url = 'https://en.wikipedia.org/wiki/English_language'
+# article = Article(url)
+# article.download()
+# article.parse()
+# article.nlp()
 
-text= article.summary
-authors=article.authors
-print(text)
-print(authors)
+# text= article.summary
+# authors=article.authors
+# print(text)
+# print(authors)
+
+# now we do it for our text
+
+with open('my_text.txt', 'r') as f:
+    text = f.read()
+
+
+
+blob=TextBlob(text) # analyzer=NaiveBayesAnalyzer()) # this line of code initializes the TextBlob and NaiveBayesAnalyzer is used for sentiment analysis
+sentiment=blob.sentiment.polarity # this line of code calculates the sentiment polarity of the text
+print(sentiment)
